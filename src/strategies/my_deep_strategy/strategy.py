c96f658826ce3da257e080e9c834fdc075565702
Wouter de Winter <wouter.de.winter@artificialindustry.com>
2018-11-15 21:09:14 +0100

    Update deep learning notebook and strategies to new file structure

--- src/strategies/my_deep_strategy/strategy.py
+++ src/strategies/my_deep_strategy/strategy.py
@@ -1,19 +1,21 @@
+from strategies.my_strategy.strategy import *
 from keras.models import load_model
-from strategies.base import Base
 import os
 
-class MyDeepStrategy(Base):
+
+class MyDeepStrategy(MyStrategy):
     def __init__(self, boat, env):
         super().__init__(boat, env)
         filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'my-keras-model.h5')
         self._model = load_model(filename)
 
     def update(self):
-        ### CREATE FEATURE DATAFRAME OR NUMPY ARRAY ###
+        ### CREATE FEATURE DATAFRAME OR NUMPY ARRAY HERE ###
         x = ...
         pred = self._model.predict(x)
 
         # cast to float because output is numpy array
         angle = float(pred[0])
 
+        # set the rudder angle
         self._boat.set_target_rudder_angle(angle)
